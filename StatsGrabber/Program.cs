3b9d8ca5aab91262aaedd0c912a47645febd2717
Kyle Webster <kyle_webster@qbsol.com>
2018-12-12 17:34:39 -0600

    Adding a bunch of stuff from the other challonge project

---
+++ StatsGrabber/Program.cs
@@ -0,0 +1,24 @@
+﻿using ChallongeApiHelper;
+using ChallongeEntities.Tournament;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StatsGrabber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string thing = ConfigurationManager.AppSettings["ApiUsername"];
+            ChallongeHttpHelper.setAuthorizationHeader(ConfigurationManager.AppSettings["ApiUsername"], ConfigurationManager.AppSettings["ApiPassword"]);
+            var getResponse = ChallongeHttpHelper.BasicGet("tournaments.json");
+
+            List<TournamentHolderGarbage> tournamentList = JsonConvert.DeserializeObject<List<TournamentHolderGarbage>>(getResponse);
+        }
+    }
+}
