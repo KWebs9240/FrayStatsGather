08a9e5ad980b2d916f009e8f5005a6e0773c1241
Kyle Webster <kyle_webster@qbsol.com>
2018-12-18 16:15:49 -0600

    Loops through to get info for every match.  Currently takes 30 seconds.  Cache to get quicker

--- StatsGrabber/Program.cs
+++ StatsGrabber/Program.cs
@@ -5,6 +5,7 @@ using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,13 +21,19 @@ namespace StatsGrabber
 
             List<TournamentRetrieval> tournamentList = ChallongeHttpHelper.GetAllTournaments();
 
-            int tournamentId = tournamentList.Last().id;
+            Dictionary<int, List<MatchRetrieval>> tournyIdMatchDict = new Dictionary<int, List<MatchRetrieval>>();
+            Dictionary<int, List<ParticipantRetrieval>> tournyIdParticipantDict = new Dictionary<int, List<ParticipantRetrieval>>();
 
-            List<MatchRetrieval> matchList = ChallongeHttpHelper.GetTournamentMatches(tournamentId);
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
 
-            List<ParticipantRetrieval> participantList = ChallongeHttpHelper.GetTournamentParticipants(tournamentId);
+            foreach(TournamentRetrieval tournament in tournamentList)
+            {
+                tournyIdMatchDict.Add(tournament.id, ChallongeHttpHelper.GetTournamentMatches(tournament.id));
+                tournyIdParticipantDict.Add(tournament.id, ChallongeHttpHelper.GetTournamentParticipants(tournament.id));
+            }
 
-            ParticipantRetrieval winner = ChallongeStaticHelpers.DetermineTournamentWinner(matchList, participantList);
+            sw.Stop();
         }
     }
 }
