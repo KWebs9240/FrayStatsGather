09c82fa2efdcbaf4ce1ca20d9ccb6f0c707b8d3e
KWebs9240 <kwarriormonk@gmail.com>
2019-03-13 00:31:19 -0500

    Getting the link sending, no preview though

--- FraytaBot.Web/Controllers/MessagesController.cs
+++ FraytaBot.Web/Controllers/MessagesController.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Web.Http;
-
+using ChallongeEntities;
 using Microsoft.Bot.Connector;
 using Microsoft.Bot.Connector.Teams;
 using Microsoft.Bot.Connector.Teams.Models;
@@ -25,13 +27,32 @@ namespace FraytaBot.Web.Controllers
 
         public static async Task TestAlive(ConnectorClient connector, Activity activity)
         {
-            var channelThing = activity.GetChannelData<TeamsChannelData>();
+            try
+            {
+                if(!activity.Type.Equals("message"))
+                {
+                    return;
+                }
 
-            //Not going to be able to tag a team or channel until this is potenitally fixed
-            //https://github.com/OfficeDev/BotBuilder-MicrosoftTeams/issues/139
-            Activity reply = activity.CreateReply($"Testing Alive");
+                List<TournamentRetrieval> recentTournaments = ChallongeApiHelper.HttpHelper.ChallongeHttpHelper.GetRecentTournaments();
 
-            await connector.Conversations.ReplyToActivityWithRetriesAsync(reply);
+                var final = recentTournaments.Last();
+
+                var channelThing = activity.GetChannelData<TeamsChannelData>();
+
+                //Not going to be able to tag a team or channel until this is potenitally fixed
+                //https://github.com/OfficeDev/BotBuilder-MicrosoftTeams/issues/139
+                Activity reply = activity.CreateReply($"The most recent tournament I probably found is\n\n[Cash Money]({final.sign_up_url}) ");
+
+                
+                await connector.Conversations.ReplyToActivityWithRetriesAsync(reply);
+            }
+            catch(Exception ex)
+            {
+                Activity reply = activity.CreateReply(ex.Message);
+
+                await connector.Conversations.ReplyToActivityWithRetriesAsync(reply);
+            }
         }
     }
 }
